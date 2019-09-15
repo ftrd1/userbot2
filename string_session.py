86fab4c27ae99a760e8b365ae29e510fe38c3ba2
ftrd1 <55282124+ftrd1@users.noreply.github.com>
2019-09-15 10:39:22 +0530

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -1,11 +1,16 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-from userbot import API_KEY, API_HASH
+from userbot import 978227
+, 
+cb6d622ea5d1734f3502ee06cd591cc4
+
 
 print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details""")
 
-with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
+with TelegramClient(StringSession(), 978227, 
+cb6d622ea5d1734f3502ee06cd591cc4
+) as client:
     print(client.session.save())
